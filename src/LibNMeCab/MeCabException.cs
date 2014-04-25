760b2fa8dc297159c795a6a447463b965b625ec2
komutan <t_komuta@nifty.com>
2014-04-25 10:09:27 +0900

    コード分析による警告へ対応

--- src/LibNMeCab/MeCabException.cs
+++ src/LibNMeCab/MeCabException.cs
@@ -3,11 +3,11 @@
 //  Copyright(C) 2001-2006 Taku Kudo <taku@chasen.org>
 //  Copyright(C) 2004-2006 Nippon Telegraph and Telephone Corporation
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace NMeCab
 {
+    [Serializable]
     public class MeCabException : Exception
     {
         public MeCabException(string message)
@@ -16,6 +16,10 @@ namespace NMeCab
 
         public MeCabException(string message, Exception ex)
             : base(message, ex)
+        { }
+
+        public MeCabException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         { }
     }
 }
