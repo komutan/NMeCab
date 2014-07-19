b217cdf676dd429ff9a40de32f0f8ca121f22b92
komutan <t_komuta@nifty.com>
2014-07-19 19:36:05 +0900

    PrioarityQueueでのBinalyHeepアルゴリズムを試行

--- src/LibNMeCab/Core/PriorityQueue.cs
+++ src/LibNMeCab/Core/PriorityQueue.cs
@@ -19,6 +19,85 @@ namespace NMeCab.Core
             this.list.Clear();
         }
 
+#if BinaryHeapPriorityQueue
+        
+        public void Push(T item)
+        {
+            int currentPos = this.list.Count;
+            this.list.Add(item);
+
+            while (currentPos != 0) // has more parrent
+            {
+                int parentPos = (currentPos - 1) / 2;
+                T parent = this.list[parentPos];
+
+                if (parent.CompareTo(item) <= 0) break; // parent is higher or equal
+
+                this.list[parentPos] = item;
+                this.list[currentPos] = parent;
+
+                currentPos = parentPos;
+            }
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0) throw new InvalidOperationException("Empty");
+
+            T ret = this.list[0];
+            this.DeleteTop();
+            return ret;
+        }
+
+        private void DeleteTop()
+        {
+            int tailPos = this.list.Count - 1;
+            this.list[0] = this.list[tailPos];
+            this.list.RemoveAt(tailPos);
+            if (tailPos == 0) return;
+            tailPos--;
+            
+            int currentPos = 0;
+            T current = this.list[0];
+            while (true)
+            {
+                int leftPos = currentPos * 2 + 1;
+                if (leftPos > tailPos) break;
+                int rightPos = leftPos + 1;
+
+                int chiledPos;
+                T chiled;
+                if (rightPos > tailPos)
+                {
+                    chiledPos = leftPos;
+                    chiled = this.list[chiledPos];
+                }
+                else
+                {
+                    T left = this.list[leftPos];
+                    T right = this.list[rightPos];
+                    if (left.CompareTo(right) < 0)
+                    {
+                        chiledPos = leftPos;
+                        chiled = left;
+                    }
+                    else
+                    {
+                        chiledPos = rightPos;
+                        chiled = right;
+                    }
+                }
+
+                if (current.CompareTo(chiled) < 0) break;
+                this.list[currentPos] = chiled;
+                this.list[chiledPos] = current;
+                current = chiled;
+                currentPos = chiledPos;
+            }
+        }
+
+#else
+
         public int Push(T item)
         {
             int index = this.SearchIndex(this.list, item);
@@ -56,5 +135,7 @@ namespace NMeCab.Core
 
             return head;
         }
+
+#endif
     }
 }
