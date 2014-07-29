ea0d2ede74734bafe8ee15be6a5c7927b20e492c
komutan <t_komuta@nifty.com>
2014-07-29 23:52:15 +0900

    テストコード

---
+++ src/LibNMeCabTest/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibNMeCabTest
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var target = new NMeCab.Core.PriorityQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    target.Push(j);
+                }
+            }
+
+            int count = target.Count;
+            int wrk1 = 0;
+            for (int k = 0; k < count; k++)
+            {
+                int wrk2 = target.Pop();
+                Assert.IsTrue(wrk1 <= wrk2);
+                wrk1 = wrk2;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var target = new NMeCab.Core.PriorityQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 10; j >= 0; j--)
+                {
+                    target.Push(j);
+                }
+            }
+
+            int count = target.Count;
+            int wrk1 = 0;
+            for (int k = 0; k < count; k++)
+            {
+                int wrk2 = target.Pop();
+                Assert.IsTrue(wrk1 <= wrk2);
+                wrk1 = wrk2;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var target = new NMeCab.Core.PriorityQueue<int>();
+            var rnd = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int count = rnd.Next(10);
+                for (int j = 0; j < count; j++)
+                {
+                    target.Push(rnd.Next(10));
+                }
+
+                count = rnd.Next(target.Count);
+                int wrk1 = 0;
+                for (int k = 0; k < count; k++)
+                {
+                    int wrk2 = target.Pop();
+                    Assert.IsTrue(wrk1 <= wrk2);
+                    wrk1 = wrk2;
+                }
+            }
+        }
+    }
+}
---
+++ src/LibNMeCabTest/UnitTest2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMeCab.Extension;
+
+namespace LibNMeCabTest
+{
+    [TestClass]
+    public class MeCabNodeExtensionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var node = new NMeCab.MeCabNode()
+            {
+                Feature = "品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音"
+            };
+            Assert.AreEqual(node.GetPartsOfSpeech(), "品詞");
+            Assert.AreEqual(node.GetPartsOfSpeechSection1(), "品詞細分類1");
+            Assert.AreEqual(node.GetPartsOfSpeechSection2(), "品詞細分類2");
+            Assert.AreEqual(node.GetPartsOfSpeechSection3(), "品詞細分類3");
+            Assert.AreEqual(node.GetConjugatedForm(), "活用形");
+            Assert.AreEqual(node.GetInflection(), "活用型");
+            Assert.AreEqual(node.GetOriginalForm(), "原形");
+            Assert.AreEqual(node.GetReading(), "読み");
+            Assert.AreEqual(node.GetPronounciation(), "発音");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var node = new NMeCab.MeCabNode()
+            {
+                Feature = "品詞"
+            };
+            Assert.AreEqual(node.GetPartsOfSpeech(), "品詞");
+            Assert.IsNull(node.GetPartsOfSpeechSection1());
+            Assert.IsNull(node.GetPartsOfSpeechSection2());
+            Assert.IsNull(node.GetPartsOfSpeechSection3());
+            Assert.IsNull(node.GetConjugatedForm());
+            Assert.IsNull(node.GetInflection());
+            Assert.IsNull(node.GetOriginalForm());
+            Assert.IsNull(node.GetReading());
+            Assert.IsNull(node.GetPronounciation());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var node = new NMeCab.MeCabNode()
+            {
+                Feature = null
+            };
+            Assert.IsNull(node.GetPartsOfSpeech());
+            Assert.IsNull(node.GetPartsOfSpeechSection1());
+            Assert.IsNull(node.GetPartsOfSpeechSection2());
+            Assert.IsNull(node.GetPartsOfSpeechSection3());
+            Assert.IsNull(node.GetConjugatedForm());
+            Assert.IsNull(node.GetInflection());
+            Assert.IsNull(node.GetOriginalForm());
+            Assert.IsNull(node.GetReading());
+            Assert.IsNull(node.GetPronounciation());
+        }
+    }
+}
