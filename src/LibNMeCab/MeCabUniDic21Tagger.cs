ad9404ef37505fd7e84f9e70f7d8db8055f2cd87
komutan <t_komuta@nifty.com>
2020-05-23 20:47:18 +0900

    Issue MeCabUniDicNode is incompatible with UniDic 2.2.0 or later #15 - 関連するテストコードを追加。そのためにUniDicも追加。（大きなバイナリなのでGIT LFSを使用） - UniDic関連のクラス名は、UniDicの特定バージョンに限定することを示すため、バージョン番号を含むものに変更。

--- src/LibNMeCab/MeCabUniDic21Tagger.cs
+++ src/LibNMeCab/MeCabUniDic21Tagger.cs
@@ -1,14 +1,14 @@
 ﻿namespace NMeCab
 {
     /// <summary>
-    /// UniDic形式の辞書を使用する場合の形態素解析処理の起点を表します。
+    /// UniDic（ver2.1.0以後）形式の辞書を使用する場合の形態素解析処理の起点を表します。
     /// </summary>
-    public class MeCabUniDicTagger : MeCabTaggerBase<MeCabUniDicNode>
+    public class MeCabUniDic21Tagger : MeCabTaggerBase<MeCabUniDic21Node>
     {
         /// <summary>
         /// コンストラクタ（非公開）
         /// </summary>
-        private MeCabUniDicTagger()
+        private MeCabUniDic21Tagger()
         { }
 
         /// <summary>
@@ -17,18 +17,18 @@
         /// <param name="dicDir">使用する辞書のディレクトリへのパス</param>
         /// <param name="userDics">使用するユーザー辞書のファイル名のコレクション</param>
         /// <returns>形態素解析処理の起点</returns>
-        public static MeCabUniDicTagger Create(string dicDir, string[] userDics = null)
+        public static MeCabUniDic21Tagger Create(string dicDir, string[] userDics = null)
         {
-            return Create(dicDir, userDics, () => new MeCabUniDicTagger());
+            return Create(dicDir, userDics, () => new MeCabUniDic21Tagger());
         }
 
         /// <summary>
         /// 形態素ノードインスタンス生成メソッドです。（内部用）
         /// </summary>
         /// <returns>形態素ノード</returns>
-        protected override MeCabUniDicNode CreateNewNode()
+        protected override MeCabUniDic21Node CreateNewNode()
         {
-            return new MeCabUniDicNode();
+            return new MeCabUniDic21Node();
         }
     }
 }
---
+++ src/LibNMeCab/MeCabUniDic22Tagger.cs
@@ -0,0 +1,34 @@
+﻿namespace NMeCab
+{
+    /// <summary>
+    /// UniDic（ver2.2.0以後）形式の辞書を使用する場合の形態素解析処理の起点を表します。
+    /// </summary>
+    public class MeCabUniDic22Tagger : MeCabTaggerBase<MeCabUniDic22Node>
+    {
+        /// <summary>
+        /// コンストラクタ（非公開）
+        /// </summary>
+        private MeCabUniDic22Tagger()
+        { }
+
+        /// <summary>
+        /// 形態素解析処理の起点を作成します。
+        /// </summary>
+        /// <param name="dicDir">使用する辞書のディレクトリへのパス</param>
+        /// <param name="userDics">使用するユーザー辞書のファイル名のコレクション</param>
+        /// <returns>形態素解析処理の起点</returns>
+        public static MeCabUniDic22Tagger Create(string dicDir, string[] userDics = null)
+        {
+            return Create(dicDir, userDics, () => new MeCabUniDic22Tagger());
+        }
+
+        /// <summary>
+        /// 形態素ノードインスタンス生成メソッドです。（内部用）
+        /// </summary>
+        /// <returns>形態素ノード</returns>
+        protected override MeCabUniDic22Node CreateNewNode()
+        {
+            return new MeCabUniDic22Node();
+        }
+    }
+}
