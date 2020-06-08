2cd9fc4d9e95c65adbef5791adf342459cfc6339
Tsuyoshi KOMUTA <t_komuta@nifty.com>
2020-06-08 16:38:34 +0900

    develop testcode #5 (#20)

---
+++ src/LibNMeCabTest/CsvParseTest.cs
@@ -0,0 +1,37 @@
+﻿using NMeCab.Core;
+using Xunit;
+
+namespace LibNMeCabTest
+{
+    public class CsvParseTest
+    {
+        [Theory]
+        [InlineData(@"a", new[] { "a" })]
+        [InlineData(@"a,b", new[] { "a", "b" })]
+        [InlineData(@"a,b,c", new[] { "a", "b", "c" })]
+        [InlineData(@"a,b,c,d,e", new[] { "a", "b", "c", "d", "e" })]
+        [InlineData(@"""a""", new[] { "a" })]
+        [InlineData(@"""a"",""b""", new[] { "a", "b" })]
+        [InlineData(@"""a"",""b"",""c""", new[] { "a", "b", "c" })]
+        [InlineData(@"""a"",""b"",""c"",""d"",""e""", new[] { "a", "b", "c", "d", "e" })]
+        [InlineData(@"""a,b"",c,d,e", new[] { "a,b", "c", "d", "e" })]
+        [InlineData(@"a,""b,c,d"",e", new[] { "a", "b,c,d", "e" })]
+        [InlineData(@"a,b,c,""d,e""", new[] { "a", "b", "c", "d,e" })]
+        [InlineData(@"""a,b,c,d,e""", new[] { "a,b,c,d,e" })]
+        [InlineData(@"""a."",""b."",""c.""", new[] { "a.", "b.", "c." })]
+        [InlineData(@""".a"","".b"","".c""", new[] { ".a", ".b", ".c" })]
+        [InlineData(@""""",b,c", new[] { @"""", "b", "c" })]
+        [InlineData(@"a,"""",c", new[] { "a", @"""", "c" })]
+        [InlineData(@"a,b,""""", new[] { "a", "b", @"""" })]
+        [InlineData(@"""""a,b,c", new[] { @"""a", "b", "c" })]
+        [InlineData(@"a"""",b,c", new[] { @"a""", "b", "c" })]
+        [InlineData(@"a,""""b"""",c", new[] { "a", @"""b""", "c" })]
+        [InlineData(@"a,b,""""c", new[] { "a", "b", @"""c" })]
+        [InlineData(@"a,b,c""""", new[] { "a", "b", @"c""" })]
+        public void SplitCsvRow(string input, string[] expected)
+        {
+            var actual = StrUtils.SplitCsvRow(input, 1, 1);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
